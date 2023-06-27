c761ad2bb90207846eac3893beb49792b7793003
lucasdbr05 <lucasgabrieljake@gmail.com>
2023-06-27 13:30:52 -0300

    Integrar arquivos static ao express

--- index.js
+++ index.js
@@ -7,8 +7,18 @@ const initRouter = require("./routes/app");
 
 //app.use("/", initRouter);
 
+app.use(express.static("public"));
+
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname + "/html/login.html"));
+});
+
+app.get("/recuperar", (req, res) => {
+  res.sendFile(path.join(__dirname + "/html/recuperacao.html"));
+});
+
+app.get("/criar-conta", (req, res) => {
+  res.sendFile(path.join(__dirname + "/html/SignUp.html"));
 });
 
 app.listen(PORT, () => {
